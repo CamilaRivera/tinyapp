c74eda50bf3628147248ebb1261305a2a6451ee3
CamilaRivera <camila.riverart@gmail.com>
2019-09-04 01:22:31 +0000

    Implement user registration server behaviour

--- express_server.js
+++ express_server.js
@@ -13,27 +13,85 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
+
+function checkEmailRepetition(users, email) {
+  for (const user in users) {
+    if (users[user].email === email) {
+      return users[user];
+    }
+  }
+  return null;
+}
+
 
 function generateRandomString() {
   return Math.random().toString(36).substring(2, 5) + Math.random().toString(36).substring(2, 5);
 }
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase, username: req.cookies.username };
+  let templateVars = { urls: urlDatabase, user: users[req.cookies.user_id] };
   res.render("urls_index", templateVars);
 });
 
+app.get("/register", (req, res) => {
+  let templateVars = { user: users[req.cookies.user_id] };
+  res.render("registration", templateVars);
+});
+
+app.get("/login", (req, res) => {
+  let templateVars = { user: users[req.cookies.user_id] };
+  res.render("login", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  if(checkEmailRepetition(users, req.body.email) !== null && req.body.password === checkEmailRepetition(users, req.body.email).password) {
+    res.cookie('user_id',  checkEmailRepetition(users, req.body.email).id);
+    res.redirect('/urls/');
+  } else {
+    res.status(403);
+    res.send('None shall pass');
+  } 
+});
+
+app.post("/register", (req, res) => {
+  if (req.body.email === "" || req.body.password === "" || checkEmailRepetition(users, req.body.email) !== null) {
+    res.status(400);
+    res.send('None shall pass');
+  } else {
+    let randomId = generateRandomString();
+    users[randomId] = {
+      id: randomId,
+      email: req.body.email,
+      password: req.body.password
+    };
+    res.cookie('user_id', randomId);
+    res.redirect("urls_index");
+  }
+});
+
 app.get("/urls/new", (req, res) => {
-  let templateVars = {username: req.cookies.username};
+  let templateVars = { user: users[req.cookies.user_id] };
   res.render("urls_new", templateVars);
 });
 
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
-  if(!req.body.longURL.startsWith('http://') && !req.body.longURL.startsWith('https://') ){
-    urlDatabase[randomString] = 'http://' +  req.body.longURL;
-  } 
+  if (!req.body.longURL.startsWith('http://') && !req.body.longURL.startsWith('https://')) {
+    urlDatabase[randomString] = 'http://' + req.body.longURL;
+  }
   console.log(urlDatabase);  // Log the POST request body to the console
   res.redirect('/urls/' + randomString);
 });
@@ -44,12 +102,12 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 });
 
 app.get("/u/:shortURL", (req, res) => {
- const longURL = urlDatabase[req.params.shortURL];
- res.redirect(longURL);
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies.username };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: users[req.cookies.user_id] };
   res.render("urls_show", templateVars);
 });
 
@@ -58,13 +116,8 @@ app.post("/urls/:shortURL/edit", (req, res) => {
   res.redirect('/urls/');
 });
 
-app.post("/login", (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls/');
-});
-
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls/');
 });
 
@@ -77,5 +130,5 @@ app.get("*", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`TinyApp listening on port ${PORT}!`);
 });
